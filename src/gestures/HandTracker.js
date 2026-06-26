ded1b82e0321903f40a1e39b763e5cb9ee93e301
3577006513-crypto <3577006513@qq.com>
2026-06-26 21:25:36 +0800

    update: fixed gesture tracking

--- src/gestures/HandTracker.js
+++ src/gestures/HandTracker.js
@@ -23,11 +23,16 @@ export class HandTracker {
     this.video = video;
     this.onResults = onResults;
     this.hands = null;
+    this.isMobileShell = /Android|MicroMessenger|MQQBrowser/i.test(navigator.userAgent);
+    this.inputCanvas = document.createElement('canvas');
+    this.inputCanvas.width = this.isMobileShell ? 320 : CONFIG.camera.width;
+    this.inputCanvas.height = this.isMobileShell ? 240 : CONFIG.camera.height;
+    this.inputContext = this.inputCanvas.getContext('2d', { alpha: false, willReadFrequently: false });
     this.stream = null;
     this.running = false;
     this.processing = false;
     this.lastSend = 0;
-    this.frameInterval = /Android|MicroMessenger|MQQBrowser/i.test(navigator.userAgent) ? 50 : 34;
+    this.frameInterval = this.isMobileShell ? 66 : 34;
     this.loop = this.loop.bind(this);
   }
 
@@ -38,9 +43,9 @@ export class HandTracker {
     });
     this.hands.setOptions({
       maxNumHands: 2,
-      modelComplexity: 1,
-      minDetectionConfidence: 0.68,
-      minTrackingConfidence: 0.72,
+      modelComplexity: this.isMobileShell ? 0 : 1,
+      minDetectionConfidence: this.isMobileShell ? 0.5 : 0.68,
+      minTrackingConfidence: this.isMobileShell ? 0.5 : 0.72,
       selfieMode: true
     });
     this.hands.onResults((results) => this.onResults(results));
@@ -49,13 +54,16 @@ export class HandTracker {
       audio: false,
       video: {
         facingMode: 'user',
-        width: { ideal: CONFIG.camera.width },
-        height: { ideal: CONFIG.camera.height },
-        frameRate: { ideal: CONFIG.camera.targetFps, max: 30 }
+        width: { ideal: this.isMobileShell ? 480 : CONFIG.camera.width },
+        height: { ideal: this.isMobileShell ? 360 : CONFIG.camera.height },
+        frameRate: { ideal: this.isMobileShell ? 24 : CONFIG.camera.targetFps, max: 30 }
       }
     });
     this.video.srcObject = this.stream;
+    this.video.setAttribute('playsinline', 'true');
+    this.video.setAttribute('webkit-playsinline', 'true');
     await this.video.play();
+    await this.#waitForVideoFrame();
     this.running = true;
     requestAnimationFrame(this.loop);
   }
@@ -63,15 +71,35 @@ export class HandTracker {
   async loop(now) {
     if (!this.running) return;
     requestAnimationFrame(this.loop);
-    if (this.processing || now - this.lastSend < this.frameInterval || this.video.readyState < 2) return;
+    if (this.processing || now - this.lastSend < this.frameInterval || this.video.readyState < 2 || !this.video.videoWidth) return;
 
     this.processing = true;
     this.lastSend = now;
     try {
-      await this.hands.send({ image: this.video });
+      const image = this.#getInputFrame();
+      await this.hands.send({ image });
     } finally {
       this.processing = false;
     }
+  }
+
+  #getInputFrame() {
+    if (!this.isMobileShell) return this.video;
+    this.inputContext.save();
+    this.inputContext.scale(-1, 1);
+    this.inputContext.drawImage(this.video, -this.inputCanvas.width, 0, this.inputCanvas.width, this.inputCanvas.height);
+    this.inputContext.restore();
+    return this.inputCanvas;
+  }
+
+  #waitForVideoFrame() {
+    if (this.video.videoWidth && this.video.videoHeight) return Promise.resolve();
+    return new Promise((resolve) => {
+      const done = () => resolve();
+      this.video.addEventListener('loadedmetadata', done, { once: true });
+      this.video.addEventListener('canplay', done, { once: true });
+      setTimeout(done, 1200);
+    });
   }
 
   stop() {
